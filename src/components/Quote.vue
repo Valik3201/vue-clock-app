e60ea2fead4f398f276bf62be8c1f788207f99de
Valik <valik3201@gmail.com>
2024-04-26 11:52:54 +0200

    feat(component): add Quote component ✨

---
+++ src/components/Quote.vue
@@ -0,0 +1,37 @@
+<template>
+  <div v-for="quote in data" :key="quote.id">
+    <p v-for="quote in data">"{{ quote.content }}"</p>
+    <p v-for="quote in data">{{ quote.author }}</p>
+
+    <button @click="getRandomQuote">Refresh</button>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      data: [],
+    };
+  },
+  mounted() {
+    this.getRandomQuote();
+  },
+  methods: {
+    getRandomQuote() {
+      const apiUrl = "https://api.quotable.io/quotes/random";
+
+      axios
+        .get(apiUrl)
+        .then((response) => {
+          this.data = response.data;
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+        });
+    },
+  },
+};
+</script>
