a00dc6f8d9dbf32a9c258b2292ff29e7f1dbb4e2
Valik <valik3201@gmail.com>
2024-04-26 11:53:01 +0200

    feat(component): add Location component ✨

---
+++ src/components/Location.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <p>In {{ city }}, {{ country }}</p>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      city: [],
+      country: [],
+    };
+  },
+  mounted() {
+    this.getLocation();
+  },
+  methods: {
+    getLocation() {
+      const apiUrl = "http://ip-api.com/json/";
+
+      axios
+        .get(apiUrl)
+        .then((response) => {
+          this.city = response.data.city;
+          this.country = response.data.country;
+        })
+        .catch((error) => {
+          console.error("Error fetching location:", error);
+          this.city = "Unknown";
+          this.country = "Unknown";
+        });
+    },
+  },
+};
+</script>
