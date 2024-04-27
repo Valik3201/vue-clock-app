62cc122f1fddeec5488ef9a008d01a6e9004fce2
Valik <valik3201@gmail.com>
2024-04-27 18:29:27 +0200

    feat(component): update `Quote` component with styles

--- src/components/Quote.vue
+++ src/components/Quote.vue
@@ -1,16 +1,26 @@
 <template>
-  <div v-for="quote in data" :key="quote.id">
-    <p v-for="quote in data">"{{ quote.content }}"</p>
-    <p v-for="quote in data" class="font-bold">{{ quote.author }}</p>
+  <div v-for="quote in data" :key="quote.id" class="container">
+    <div class="flex items-start gap-4 text-h5">
+      <div class="max-w-[33.75rem]">
+        <p v-for="quote in data" class="mb-3">"{{ quote.content }}"</p>
+        <p v-for="quote in data" class="font-bold">{{ quote.author }}</p>
+      </div>
 
-    <button @click="getRandomQuote">Refresh</button>
+      <button @click="getRandomQuote" class="pt-1">
+        <icon name="refresh" />
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import Icon from "./Icon.vue";
 
 export default {
+  components: {
+    Icon,
+  },
   data() {
     return {
       data: [],
