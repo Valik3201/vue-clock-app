a1a16b1f54711c76ed1a86c3199c306db242bc69
Valik <valik3201@gmail.com>
2024-04-27 18:28:29 +0200

    refactor(icons): replace SVG icons with Vue components

---
+++ src/components/Icon.vue
@@ -0,0 +1,24 @@
+<template>
+  <component :is="dynamicComponent" />
+</template>
+
+<script>
+import { defineAsyncComponent } from "vue";
+
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+
+  computed: {
+    dynamicComponent() {
+      const name = this.name.charAt(0).toUpperCase() + this.name.slice(1);
+
+      return defineAsyncComponent(() => import(`../icons/${name}.vue`));
+    },
+  },
+};
+</script>
