1816ffae7f1bf6fe937ff99b907de5873b539c17
Valik <valik3201@gmail.com>
2024-04-27 19:40:42 +0200

    feat(ui): аdd text logo `clock.`

--- src/components/Quote.vue
+++ src/components/Quote.vue
@@ -1,14 +1,17 @@
 <template>
   <div v-for="quote in data" :key="quote.id" class="container">
-    <div class="flex items-start gap-4 text-h5">
-      <div class="max-w-[33.75rem]">
-        <p v-for="quote in data" class="mb-3">"{{ quote.content }}"</p>
-        <p v-for="quote in data" class="font-bold">{{ quote.author }}</p>
-      </div>
+    <div class="flex justify-between">
+      <div class="flex items-start gap-4 text-h5">
+        <div class="max-w-[33.75rem]">
+          <p v-for="quote in data" class="mb-3">"{{ quote.content }}"</p>
+          <p v-for="quote in data" class="font-bold">{{ quote.author }}</p>
+        </div>
 
-      <button @click="getRandomQuote" class="pt-1">
-        <icon name="refresh" />
-      </button>
+        <button @click="getRandomQuote" class="pt-1">
+          <icon name="refresh" />
+        </button>
+      </div>
+      <p class="text-h2 font-bold">clock.</p>
     </div>
   </div>
 </template>
