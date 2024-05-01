0f3a23671962352ac2eb347db2accfd044151caa
Valik <valik3201@gmail.com>
2024-05-01 14:19:02 +0200

    style(css): update line height 💄

--- src/components/TimeDisplay.vue
+++ src/components/TimeDisplay.vue
@@ -3,7 +3,9 @@
     <h1 class="font-bold text-h1-3 md:text-h1-2 lg:text-h1">
       {{ formattedTime }}
     </h1>
-    <p class="font-light text-h6 md:text-[2rem] lg:text-abbreviation">
+    <p
+      class="font-light text-h6 md:text-[2rem] lg:text-abbreviation lg:leading-loose"
+    >
       {{ abbreviation }}
     </p>
   </div>
