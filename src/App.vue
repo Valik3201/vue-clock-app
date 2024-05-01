25ab2ed714430b2f2fdd00a77d85a869a016a007
Valik <valik3201@gmail.com>
2024-05-01 13:33:02 +0200

    feat(ui): add font import in `App.vue` 💄

--- src/App.vue
+++ src/App.vue
@@ -5,3 +5,7 @@
 <script setup lang="ts">
 import Time from "./components/Time.vue";
 </script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap");
+</style>
